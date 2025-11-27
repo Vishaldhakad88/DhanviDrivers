48524401160048e98fff8df362c54a23e9891073
Vishal Dhakad <vishal@acoreithub.com>
2025-11-27 15:28:30 +0530

    ddddddd

--- src/components/Press.jsx
+++ src/components/Press.jsx
@@ -3,9 +3,9 @@ import { motion } from "framer-motion";
 
 // FIXED: Correct paths & extensions
 // import press1 from "../assets/press/press1.jpg";
-import press1 from "../assets/press/press1"
-import press2 from "../assets/press/press2.jpg";
-import press3 from "../assets/press/press3.jpg";
+import press1 from "../assets/press1.jpg"
+import press2 from "../assets/press2.jpg";
+import press3 from "../assets/press3.jpg";
  
 export default function PressSection() {
   const pressData = [
